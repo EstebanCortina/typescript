0965786f197621bc1c96a451a7e4e5ff56f52640
Esteban Cortina <estebannsebastian@gmail.com>
2023-03-30 20:29:27 -0600

    Project create

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let message = "This is a message from Typescript";
+message = "New message";
+console.log(message);
+message = "Another message";
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+let message: string = "This is a message from Typescript";
+
+message = "New message";
+
+console.log(message);
+message = "Another message";

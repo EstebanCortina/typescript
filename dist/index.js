9749a801eda19822785b7156fa7333dfe4911fcb
Esteban Cortina <estebannsebastian@gmail.com>
2023-03-30 22:14:53 -0600

    Definicion de tipos

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,15 @@
 "use strict";
 let message = "This is a message from Typescript";
-message = "New message";
-console.log(message);
-message = "Another message";
+let message2 = "Message from another variable";
+console.log(typeof message);
+console.log(typeof message2);
+function exampleFunction(param) {
+    return param;
+}
+let array1 = ["One", "Two", "Three"];
+let array2 = [1, 2, 3];
+let array1V2 = ["One", "Two", "Three"];
+let array2V2 = [1, 2, 3];
+array1.map(x => console.log(x.toUpperCase));
+let tup = [1, "Second value of the tuple"];
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,31 @@
+//The data types of the variables can be declared from a explicit way or with an inference by the value assigned
+
 let message: string = "This is a message from Typescript";
+let message2 = "Message from another variable";
 
-message = "New message";
+//Both are strings
+console.log(typeof message);
+console.log(typeof message2);
 
-console.log(message);
-message = "Another message";
+
+//The functions also need to specify the data type of the params
+function exampleFunction(param: number) {
+  return param;
+}
+
+//There is two ways for arrays data type declarations
+let array1: string[] = ["One", "Two", "Three"];
+let array2: number[] = [1, 2, 3];
+
+let array1V2: Array<string> = ["One", "Two", "Three"];
+let array2V2: Array<number> = [1, 2, 3];
+
+//When we are working with different variables, on certain moments could happen we don´t know which data type a variable is, so TS recognizes de type and throws a bunch o methods for that specific type.
+
+array1.map(x => console.log(x.toUpperCase));//It knows array1 is a string container and suggest string methods.
+
+//Tuple is like fields of a data base table. You need to insert the values in the exact same order that the fields are. On this case, you need to put a number and then a string, but we can add more data types to the declaration like "tup:[number, string, number,...]" and so on..., but this is not recommended.
+//( WARNING: There´s no error for .push() to a tuple)
+
+let tup: [number, string] = [1, "Second value of the tuple"];
+
